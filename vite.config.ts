7d431c1cd4fd3fd66ce9f4e7fa64c129b4e8e1e8
zzh <465777242@qq.com>
2026-01-26 17:08:53 +0800

    ‘fix:部署配置’

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
-    base: '/jiale_admin/',
+    base: '/',
     plugins: [vue()],
     resolve: {
       alias: {
